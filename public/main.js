2bf245c258d221d4ad79dc5eb034ce6caea8ddff
kissa1001 <kissa101295@gmail.com>
2016-03-12 00:20:42 -0500

    chatbox generated

--- public/main.js
+++ public/main.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
     var socket = io();
+    var square = document.getElementsByClassName("square");
+    var num_ship = 10;
+    var fields = 100;
+    input = $('.msgInput');
+    var nickInput = $('.usernameInput');
+    var loginPage = $('.login-form'); 
+    var chatPage = $('.wrapper'); 
+    var messages = $('#messages')
+    var nickname;
 
+    //Scrollbar
+    $("#chat").addClass("thin");
 
-var square = document.getElementsByClassName("square");
-var num_ship = 10;
-var fields = 100;
-
+    $("#chat").mouseover(function(){
+        $(this).removeClass("thin");
+    });
+    $("#chat").mouseout(function(){
+        $(this).addClass("thin");
+    });
+    $("#chat").scroll(function () {
+        $("#chat").addClass("thin");
+    });
 
     //add empty fields to our game
     addEmptyFields(fields);
@@ -25,4 +41,58 @@ var fields = 100;
           $('.myShips').append(divSquare);
       };
     }
+
+    //Add message
+    var addMessage = function(message) {
+        messages.append('<div>' + message + '</div>');
+    };
+
+    //Create username
+    socket.on('connect', function(data){
+        function setUsername () {
+            nickname = nickInput.val();
+            // If the username is valid
+            if (nickname) {
+                loginPage.fadeOut();
+                socket.emit('join', nickname);
+                chatPage.show();
+            }
+        }     
+        nickInput.on('keydown',function (event) {
+            // When the client hits ENTER on their keyboard
+            if (event.which === 13) {
+                setUsername();
+            }
+        });
+    });
+    socket.on('usernames', function(name){
+        var html = '';
+        for(var i=0; i < name.length; i++){
+            html += name[i] + '<br/>'
+        }
+        usersDiv.html(html);
+    });
+    //User is typing
+    $("input").on("keyup", function (event) {
+        socket.emit("sender", {
+            nickname: nickname
+        });
+    });
+    socket.on("sender", function (data) {
+        $("#status").html(data.nickname + " is typing");
+        setTimeout(function () {
+            $("#status").html('');
+        }, 3000);
+    });
+
+    input.on('keydown', function(event) {
+        if (event.keyCode != 13) {
+            return;
+        }
+
+        var message = input.val();
+        socket.emit('message', message);
+        input.val('');
+    });
+    socket.on('message',addMessage)
 });
--- server.js
+++ server.js
@@ -8,8 +8,33 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+//Connected users counter
+var connectCounter = 0;
+
 io.on('connection', function (socket) {
-    console.log('Connected');
+    //Create username
+    socket.on('join', function(name){
+    	socket.nickname = name;
+        console.log(name + ' connected');
+        connectCounter++;
+        console.log('Connected users: ' + connectCounter); 
+    });
+
+    socket.on('disconnect', function() {
+        connectCounter--;
+        console.log('Connected users: ' + connectCounter);
+    });
+    //User is typing
+    socket.on("sender", function (data) {
+        socket.broadcast.emit("sender", data); 
+    });
+
+    socket.on('message', function(message) {
+        var nickname = socket.nickname;
+        console.log(nickname + ' said:', message);
+    	socket.broadcast.emit('message', nickname + ': ' + message);
+    	socket.emit('message', 'Me: ' + message);
+    });
 });
 
 server.listen(8080);
